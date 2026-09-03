f21fb7c5895c47b99a95a270d87cdba464d10a38
Francisco Rodriguez <franrod877@gmail.com>
2026-09-03 17:38:22 -0600

    chore: initialize backend project

---
+++ src/main/java/com/admosa/backend/AdmosaBackendApplication.java
@@ -0,0 +1,13 @@
+package com.admosa.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdmosaBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdmosaBackendApplication.class, args);
+	}
+
+}
